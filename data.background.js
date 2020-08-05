dee87fa64781f7f5638fad3fbe1ad630094fa343
sultan nadim <sultan5218@gmail.com>
2020-08-05 20:39:50 +0600

    Add files via upload

---
+++ data.background.js
@@ -0,0 +1,9 @@
+
+(function ($) {
+    "use strict";
+ 
+    $("[data-background]").each(function(){
+        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+    })
+    
+})(jQuery);
